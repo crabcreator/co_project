6628d2f1e0864004a4742e15075b14a599ff9ca7
Nikita Rybalko <playnik3@gmail.com>
2024-01-07 14:58:17 +0200

    fix: file structure; feat: embedded the city-api into React Context

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,18 +1,49 @@
 import React, { useState, useEffect, useRef, useContext } from "react";
-import { CityContext, cities } from "../../Contexts/CityContext";
+import { CityContext } from "../../Contexts/CityContext";
 import { LangContext, langs } from "../../Contexts/LangContext";
 import {Link} from "react-router-dom";
+import axios from "axios"
 import "./Header.css";
 
 export default function Header() {
-    const {city, setCity} = useContext(CityContext);
+    const {
+        cityName,
+        // cityId,
+        setCity
+    } = useContext(CityContext);
     const {lang, setLang} = useContext(LangContext)
 
     const [searchIsOpen, setSearchOpen] = useState(false);
     const [langIsOpen, setLangOpen] = useState(false);
+    const [searchValue, setSearchValue] = useState("");
+    const [searchResults, setSearchResults] = useState({name: "", id: 0});
 
     let menuRef = useRef<HTMLDivElement>(null);
     let langRef = useRef<HTMLDivElement>(null);
+    let resultsRef = useRef<HTMLDivElement>(null);
+
+    const changeValue = (e:React.ChangeEvent<HTMLElement> & {target: HTMLInputElement}) => {
+        setSearchValue(e.target.value) 
+    }
+    // console.log(window.localStorage.getItem("City"))
+
+    useEffect(() => {
+        if(searchValue) {    
+            axios.get(`https://geocoding-api.open-meteo.com/v1/search?name=${searchValue}&count=1&language=ua&format=json`)
+            .then(response => {
+                if(response.data.results[0].country==="Ukraine") {
+                    setSearchResults({name: response.data.results[0].name, id: response.data.results[0].id});
+                    console.log(searchResults)
+                }
+            })
+            .catch(error => console.log(error))
+        }
+        // if(!searchValue) {
+        //     setSearchResults({name: "", id: 0});
+        // }
+    }, [searchValue])
+    
+console.log(cityName)
     
     useEffect(() => {
         let handler = (e:React.MouseEvent<HTMLElement> & {target: HTMLElement}) => {
@@ -48,7 +79,7 @@ export default function Header() {
                         <div className="location-sign">
                             <img src="/img/header/location-sign.png" alt="" />
                         </div>
-                        <span className="select-city">{city==="" ? "Вибрати місто" : city}</span>
+                        <span className="select-city">{cityName==="" ? "Вибрати місто" : cityName}</span>
                         <div className="drop-arrow">
                             <img src="/img/header/menu-drop-arrow.png" alt="" />
                         </div>
@@ -56,7 +87,18 @@ export default function Header() {
                     <div className={`dropdown-search ${searchIsOpen ? "active" : ""}`} ref={menuRef}>
                         <div className="select-city-drop">
                             <img src="/img/header/active-search.png" alt="" />
-                            <input type="text" placeholder="Місто, область" />
+                            <input type="text" placeholder="Місто, область" onChange={(e) => changeValue(e)} value={searchValue} />
+                        </div>
+                        <div className="search-results" ref={resultsRef}>
+                            <ul className="res-cities-list">
+                                <li className="res-cities-list-el"><button onClick={() => {
+                                    setCity(searchResults.name, searchResults.id);
+                                    setSearchValue("");
+                                    setSearchOpen(false);
+                                    setSearchResults({name: "", id: 0})
+                                    }}>{searchResults.name}
+                                </button></li>
+                            </ul>                          
                         </div>
                         <div className="turn-on-gps">
                             <img src="/img/header/gps.png" alt="" />
@@ -64,13 +106,13 @@ export default function Header() {
                         </div>
                         <div className="previous-cities">
                             <span>Ви нещодавно дивилися</span>
-                            <ul className="cities-list">
-                                {cities.map((el, index) => (
-                                    <li key={el} className="cities-list-el"><button onClick={() => {
+                            <ul className="last-cities-list">
+                                {/* {cities.map((el, index) => (
+                                    <li key={el} className="last-cities-list-el"><button onClick={() => {
                                         setCity(index);
                                         setSearchOpen(false);
                                     }}>{el}</button></li>
-                                ))}
+                                ))} */}
                             </ul>
                         </div>
                     </div>
